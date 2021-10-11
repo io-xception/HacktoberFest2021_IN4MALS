96f87d831316a6c668e30c8e5ae40bf1de956736
bhavy007 <44473716+bhavy007@users.noreply.github.com>
2021-10-12 01:37:00 +0530

    queue linkedlist implementation

---
+++ C/queue linkedlist implementation.c
@@ -0,0 +1,79 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void Enqueue(int x){
+    struct Node* temp =(struct Node*)malloc(sizeof(struct Node*));
+    temp->data=x;
+    temp->next=NULL;
+    if(front==NULL && rear==NULL){
+        front=rear=temp;
+        return;
+    }
+    else{
+        rear->next=temp;
+        rear=temp;
+    }
+}
+
+void Dequeue(){
+    struct Node* temp = front;
+   if(front==NULL){
+       printf("Error! Empty Queue");
+       return;
+   }
+   if(front==rear)
+   {
+       front=rear=NULL;
+   }
+   else{
+       front=front->next;
+   }
+   free(temp);
+}
+
+void Front(){
+    printf("\nFront    :   %d\n", front->data);   
+}
+
+void Print(){
+    struct Node* temp = front;
+    if(temp==NULL){
+        printf("Error! Empty Queue!");
+    }
+    printf("Queue:  ");
+    while(temp!=NULL){
+        printf("  %d  ", temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    Enqueue(2);Print();
+    Enqueue(4);Print();
+    Enqueue(5);Print();
+    Enqueue(1);Print();
+    Dequeue();Print();
+    Dequeue();Print();
+    Front();
+
+    return 0;
+}
+
